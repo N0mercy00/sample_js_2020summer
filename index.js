797552283216a9e69db37bab519105188c27dd45
N0mercy00 <dkdhehesp@naver.com>
2020-07-03 01:41:48 +0900

    click event and detect online/offline

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Im working. Im JS and super Sexy')
+const title = document.querySelector("#title");
+const BASE_COLOR= "rgb(52, 73, 94)";
+const OTHER_COLOR ="#7f8c8d";
+
+function handleClick(){
+    const currentColor = title.style.color;
+    if(currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    }else{
+        title.style.color = BASE_COLOR;
+    }
+}
+function init(){
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click",handleClick)
+}
+init();
+
+function handleOnline(){
+    console.log("System ONLINE");
+}
+
+function handleOffline(){
+    console.log("System OFFLINE");
+}
+
+window.addEventListener("online",handleOnline);
+window.addEventListener("offline",handleOffline);
+
