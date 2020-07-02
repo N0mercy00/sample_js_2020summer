9aab13d87a11959aa63246e82cf177c34cab43c0
N0mercy00 <dkdhehesp@naver.com>
2020-07-03 02:06:14 +0900

    2.7장 까지 따라하기

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 const title = document.querySelector("#title");
-const BASE_COLOR= "rgb(52, 73, 94)";
-const OTHER_COLOR ="#7f8c8d";
+const CLICKED_CLASS="clicked";
 
 function handleClick(){
-    const currentColor = title.style.color;
-    if(currentColor === BASE_COLOR){
-        title.style.color = OTHER_COLOR;
-    }else{
-        title.style.color = BASE_COLOR;
-    }
+    title.classList.toggle(CLICKED_CLASS);
+    //toggle 함수 안에 파라매터가 있는지 체크 있으면 add 없으면 remove
 }
+
 function init(){
-    title.style.color = BASE_COLOR;
     title.addEventListener("click",handleClick)
 }
+
 init();
-
-function handleOnline(){
-    console.log("System ONLINE");
-}
-
-function handleOffline(){
-    console.log("System OFFLINE");
-}
-
-window.addEventListener("online",handleOnline);
-window.addEventListener("offline",handleOffline);
 
