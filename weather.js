0fbaee585eb3ddc0e7b7bee04b8bf22fc864bb69
N0mercy00 <dkdhehesp@naver.com>
2020-07-04 21:29:10 +0900

    add) Weather and location

--- weather.js
+++ weather.js
@@ -1,5 +1,17 @@
+const weather = document.querySelector(".js-weather");
 const API_KEY = "65a94a8e1a4688b81dca195d4dd1da72";
 const COORDS = ' coords'
+
+function getWeather(lat,lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json();
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place=json.name;
+        weather.innerText=`${temperature}@${place}`
+    });
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS,JSON.stringify(coordsObj));
@@ -13,6 +25,7 @@ function handleGeoSucces(position){
         longitude: longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude,longitude);
 }
 function handleGeoError(){
     console.log('Can not access ger loacation');
@@ -23,11 +36,12 @@ function askForCoords(){
 }
 
 function loadCoords(){
-    const loadedCords = localStorage.getItem(COORDS)
-    if(loadedCords === null){
+    const loadedCoords = localStorage.getItem(COORDS)
+    if(loadedCoords === null){
         askForCoords();
     }else{
-        //get weather
+       const parsedCoords = JSON.parse(loadedCoords);
+       getWeather(parsedCoords.latitude,parsedCoords.longitude);
     }
 }
 
